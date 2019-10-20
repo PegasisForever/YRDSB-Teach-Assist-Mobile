f92accab82590b7bbe987e0ecab1a043ebdf27f9
pegasis <admin@pegasis.site>
2019-10-19 23:20:40 -0400

    ~ optimized bar chart in summary view when there's only one "other" column

--- lib/pages/detailpage/assignmentstab/SmallMarkChart.dart
+++ lib/pages/detailpage/assignmentstab/SmallMarkChart.dart
@@ -6,21 +6,33 @@ import 'package:ta/tools.dart';
 
 class _SmallMarkChartPainter extends CustomPainter {
   final Assignment _assi;
+  final bool _KTCAAvailable;
   final Color _Kcolor = Color(0xffffeb3b);
   final Color _Tcolor = Color(0xff8bc34a);
   final Color _Ccolor = Color(0xff9fa8da);
   final Color _Acolor = Color(0xffffb74d);
+  final Color _Ocolor = Color(0xff90a4ae);
 
-  _SmallMarkChartPainter(this._assi);
+  _SmallMarkChartPainter(this._assi,this._KTCAAvailable);
 
   @override
   void paint(Canvas canvas, Size size) {
     var height = size.height;
 
-    _paintBar(canvas, Strings.get("ku_single"), _Kcolor, _assi.KU, 0, 40, height);
-    _paintBar(canvas, Strings.get("t_single"), _Tcolor, _assi.T, 40, 40, height);
-    _paintBar(canvas, Strings.get("c_single"), _Ccolor, _assi.C, 80, 40, height);
-    _paintBar(canvas, Strings.get("a_single"), _Acolor, _assi.A, 120, 40, height);
+    if (_KTCAAvailable){
+      _paintBar(
+          canvas, Strings.get("ku_single"), _Kcolor, _assi.KU, 0, 40, height);
+      _paintBar(
+          canvas, Strings.get("t_single"), _Tcolor, _assi.T, 40, 40, height);
+      _paintBar(
+          canvas, Strings.get("c_single"), _Ccolor, _assi.C, 80, 40, height);
+      _paintBar(
+          canvas, Strings.get("a_single"), _Acolor, _assi.A, 120, 40, height);
+    }else{
+      _paintBar(
+          canvas, Strings.get("o_single"), _Ocolor, _assi.O, 15, 40, height);
+    }
+
   }
 
   void _paintBar(Canvas canvas, String text, Color color, SmallMark smallMark,
@@ -51,7 +63,7 @@ class _SmallMarkChartPainter extends CustomPainter {
               topRight: Radius.circular(5)),
           paint);
 
-      if (smallMark.finished){
+      if (smallMark.finished) {
         TextPainter(
             text: TextSpan(
                 text: getRoundString(mark, 1),
@@ -60,17 +72,15 @@ class _SmallMarkChartPainter extends CustomPainter {
             textAlign: TextAlign.center)
           ..layout(maxWidth: width, minWidth: width)
           ..paint(canvas, Offset(x, (height - 32) * (1 - mark / 100) - 2));
-      }else{
+      } else {
         TextPainter(
             text: TextSpan(
-                text: "N",
-                style: TextStyle(fontSize: 16.0, color: Colors.red)),
+                text: "N", style: TextStyle(fontSize: 16.0, color: Colors.red)),
             textDirection: TextDirection.ltr,
             textAlign: TextAlign.center)
           ..layout(maxWidth: width, minWidth: width)
           ..paint(canvas, Offset(x, (height - 32) - 2));
       }
-
     } else {
       TextPainter(
           text: TextSpan(
@@ -94,11 +104,15 @@ class SmallMarkChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var KTCAAvailable = _assi.KU.available ||
+        _assi.T.available ||
+        _assi.C.available ||
+        _assi.A.available;
     return Container(
       height: 100,
-      width: 160,
+      width: KTCAAvailable?160:70,
       child: CustomPaint(
-        painter: _SmallMarkChartPainter(_assi),
+        painter: _SmallMarkChartPainter(_assi,KTCAAvailable),
       ),
     );
   }
