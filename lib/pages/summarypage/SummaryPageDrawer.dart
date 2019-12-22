477c560d445fa2d932e9e47bc872c24a61fcfd93
pegasis <admin@pegasis.site>
2019-12-22 11:21:21 -0500

    + removed text in the bottom of the drawer

--- lib/pages/summarypage/SummaryPageDrawer.dart
+++ lib/pages/summarypage/SummaryPageDrawer.dart
@@ -31,120 +31,88 @@ class _SummaryPageDrawerState extends State<SummaryPageDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
+        child: ScrollConfiguration(
+      behavior: DisableOverScroll(),
+      child: ListView(
+        padding: EdgeInsets.zero,
         children: <Widget>[
-          Flexible(
-            child: ScrollConfiguration(
-              behavior: DisableOverScroll(),
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: <Widget>[
-                  UADrawerHeader.UserAccountsDrawerHeader(
-                    isOpened: _drawerHeaderOpened,
-                    accountName: Text(currentUser.displayName == ""
-                        ? currentUser.number
-                        : currentUser.displayName),
-                    accountEmail: currentUser.displayName == "" ? null : Text(currentUser.number),
-                    currentAccountPicture: CircleAvatar(
-                      child: Text(
-                        (currentUser.displayName == ""
-                            ? currentUser.number.substring(7, 9)
-                            : (currentUser.displayName.length > 2
-                                ? currentUser.displayName.substring(0, 2)
-                                : currentUser.displayName)),
-                        style: TextStyle(fontSize: 40.0),
-                      ),
-                    ),
-                    onDetailsPressed: () {
-                      setState(() {
-                        _drawerHeaderOpened = !_drawerHeaderOpened;
-                      });
-                    },
-                  ),
-                  CrossFade(
-                    firstChild: Column(
-                      children: getAccountSelectorList(),
-                    ),
-                    secondChild: Container(),
-                    showFirst: _drawerHeaderOpened,
-                  ),
-                  ListTile(
-                    title: Text(Strings.get("moodle")),
-                    leading: Image.asset(
-                      isLightMode(context: context)
-                          ? "assets/images/moodle_logo.png"
-                          : "assets/images/moodle_logo_ondark.png",
-                      height: 28,
-                      width: 28,
-                    ),
-                    onTap: () {
-                      Navigator.pop(context);
-                      openCustomTab(context, "https://moodle2.yrdsb.ca/");
-                    },
-                  ),
-                  ListTile(
-                    title: Text(Strings.get("archived_marks")),
-                    leading: Icon(Icons.archive),
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.pushNamed(context, "/archived_course");
-                    },
-                  ),
-                  ListTile(
-                    title: Text(Strings.get("feedback")),
-                    leading: Icon(Icons.message),
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.pushNamed(context, "/feedback");
-                    },
-                  ),
-                  ListTile(
-                    title: Text(Strings.get("settings")),
-                    leading: Icon(Icons.settings),
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.pushNamed(context, "/settings");
-                    },
-                  ),
-                  ListTile(
-                    title: Text(Strings.get("about")),
-                    leading: Icon(Icons.info),
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.pushNamed(context, "/about");
-                    },
-                  ),
-                ],
+          UADrawerHeader.UserAccountsDrawerHeader(
+            isOpened: _drawerHeaderOpened,
+            accountName:
+                Text(currentUser.displayName == "" ? currentUser.number : currentUser.displayName),
+            accountEmail: currentUser.displayName == "" ? null : Text(currentUser.number),
+            currentAccountPicture: CircleAvatar(
+              child: Text(
+                (currentUser.displayName == ""
+                    ? currentUser.number.substring(7, 9)
+                    : (currentUser.displayName.length > 2
+                        ? currentUser.displayName.substring(0, 2)
+                        : currentUser.displayName)),
+                style: TextStyle(fontSize: 40.0),
               ),
             ),
+            onDetailsPressed: () {
+              setState(() {
+                _drawerHeaderOpened = !_drawerHeaderOpened;
+              });
+            },
           ),
-          Flexible(
-            flex: 0,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: GestureDetector(
-                onTap: () => openCustomTab(context,"https://dev.pegasis.site/ta"),
-                child: RichText(
-                  text: TextSpan(children: [
-                    TextSpan(text: "Tap ", style: Theme.of(context).textTheme.caption),
-                    TextSpan(
-                        text: "here",
-                        style: Theme.of(context).textTheme.caption.copyWith(
-                              color: isLightMode(context: context) ? Colors.blue : Colors.blue[300],
-                              decoration: TextDecoration.underline,
-                            )),
-                    TextSpan(
-                        text:
-                            " to learn more about how this app works and how to contribute to the development.",
-                        style: Theme.of(context).textTheme.caption),
-                  ]),
-                ),
-              ),
+          CrossFade(
+            firstChild: Column(
+              children: getAccountSelectorList(),
             ),
-          )
+            secondChild: Container(),
+            showFirst: _drawerHeaderOpened,
+          ),
+          ListTile(
+            title: Text(Strings.get("moodle")),
+            leading: Image.asset(
+              isLightMode(context: context)
+                  ? "assets/images/moodle_logo.png"
+                  : "assets/images/moodle_logo_ondark.png",
+              height: 28,
+              width: 28,
+            ),
+            onTap: () {
+              Navigator.pop(context);
+              openCustomTab(context, "https://moodle2.yrdsb.ca/");
+            },
+          ),
+          ListTile(
+            title: Text(Strings.get("archived_marks")),
+            leading: Icon(Icons.archive),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.pushNamed(context, "/archived_course");
+            },
+          ),
+          ListTile(
+            title: Text(Strings.get("feedback")),
+            leading: Icon(Icons.message),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.pushNamed(context, "/feedback");
+            },
+          ),
+          ListTile(
+            title: Text(Strings.get("settings")),
+            leading: Icon(Icons.settings),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.pushNamed(context, "/settings");
+            },
+          ),
+          ListTile(
+            title: Text(Strings.get("about")),
+            leading: Icon(Icons.info),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.pushNamed(context, "/about");
+            },
+          ),
         ],
       ),
-    );
+    ));
   }
 
   List<Widget> getAccountSelectorList() {
