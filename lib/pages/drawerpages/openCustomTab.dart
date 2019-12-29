b7a5971868cba79a68b7569b3f9df86cc319d6ec
Pegasis <admin@pegasis.site>
2019-12-29 03:07:37 -0500

    + change custom tab color to white/black

--- lib/pages/drawerpages/openCustomTab.dart
+++ lib/pages/drawerpages/openCustomTab.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
-import 'package:ta/tools.dart';
 
 void openCustomTab(BuildContext context, String url) async {
   await launch(
     url,
-    option: new CustomTabsOption(
-      toolbarColor: primaryColorOf(context),
+    option: CustomTabsOption(
+      toolbarColor: Theme.of(context).canvasColor,
       enableDefaultShare: false,
       enableUrlBarHiding: true,
       showPageTitle: true,
-      animation: new CustomTabsAnimation.slideIn(),
+      animation: new CustomTabsAnimation.fade(),
       extraCustomTabs: <String>[
         'org.mozilla.firefox',
         'com.microsoft.emmx',
