cae1f6a7d5f621914f9e069749e193b8b89cc0cc
Pegasis <admin@pegasis.site>
2019-12-28 22:02:58 -0500

    + appbar in archived page won't hide if there's none data

--- lib/pages/archivedpage/ArchivedCoursesPage.dart
+++ lib/pages/archivedpage/ArchivedCoursesPage.dart
@@ -17,11 +17,23 @@ class ArchivedCoursesPage extends StatefulWidget {
 
 class _ArchivedCoursesPageState extends State<ArchivedCoursesPage>
     with AfterLayoutMixin<ArchivedCoursesPage> {
-  var _refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
+  var _noDataRefreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
+  var _haveDataRefreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
   var archivedCourses = getArchivedCourseListOf(currentUser.number);
 
   @override
   Widget build(BuildContext context) {
+    var sidePadding = (widthOf(context) - 500) / 2;
+    return archivedCourses.length == 0
+        ? _noDataView(
+            sidePadding: sidePadding,
+          )
+        : _haveDataView(
+            sidePadding: sidePadding,
+          );
+  }
+
+  Widget _haveDataView({double sidePadding}) {
     var bronze = 0;
     var silver = 0;
     var gold = 0;
@@ -35,67 +47,102 @@ class _ArchivedCoursesPageState extends State<ArchivedCoursesPage>
       }
     });
 
-    var sidePadding = (widthOf(context) - 500) / 2;
     return Scaffold(
       body: NestedScrollView(
         headerSliverBuilder: (context, innerBoxScrolled) => [
           SliverAppBar(
             title: Text(Strings.get("archived_marks")),
             forceElevated: true,
-            floating: true,
-            snap: true,
+            floating: false,
+            snap: false,
           ),
         ],
-        body: Stack(
-          children: <Widget>[
-            if (archivedCourses.length == 0)
-              Center(
-                child: Text(
-                  Strings.get(
-                    "no_archived_courses",
-                  ),
-                  style: Theme.of(context).textTheme.subhead,
-                ),
-              ),
-            RefreshIndicator(
-              key: _refreshIndicatorKey,
-              onRefresh: () async {
-                try {
-                  await getAndSaveArchived(currentUser);
-                  setState(() {
-                    archivedCourses = getArchivedCourseListOf(currentUser.number);
-                  });
-                } catch (e) {}
-              },
-              child: ListView.builder(
-                padding: EdgeInsets.only(
-                  left: max(sidePadding, 6),
-                  right: max(sidePadding, 6),
-                  bottom: MediaQuery.of(context).padding.bottom + 16,
-                ),
-                itemCount: archivedCourses.length + 1,
-                itemBuilder: (context, index) {
-                  if (index == 0) {
-                    return AwardBar(bronze, silver, gold);
-                  } else {
-                    var course = archivedCourses[index - 1];
-                    return CourseCard(
-                      course: course,
-                      onTap: () {
-                        Navigator.pushNamed(
-                          context,
-                          "/detail",
-                          arguments: [course],
-                        );
-                      },
-                      showIcons: false,
-                      showAnimations: false,
-                    );
-                  }
-                },
-              ),
+        body: RefreshIndicator(
+          key: _haveDataRefreshIndicatorKey,
+          onRefresh: () async {
+            try {
+              await getAndSaveArchived(currentUser);
+              setState(() {
+                archivedCourses = getArchivedCourseListOf(currentUser.number);
+              });
+            } catch (e) {}
+          },
+          child: ListView.builder(
+            padding: EdgeInsets.only(
+              left: max(sidePadding, 6),
+              right: max(sidePadding, 6),
+              bottom: MediaQuery.of(context).padding.bottom + 16,
             ),
-          ],
+            itemCount: archivedCourses.length + 1,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return AwardBar(bronze, silver, gold);
+              } else {
+                var course = archivedCourses[index - 1];
+                return CourseCard(
+                  course: course,
+                  onTap: () {
+                    Navigator.pushNamed(
+                      context,
+                      "/detail",
+                      arguments: [course],
+                    );
+                  },
+                  showIcons: false,
+                  showAnimations: false,
+                );
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _noDataView({double sidePadding}) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(Strings.get("archived_marks")),
+      ),
+      body: RefreshIndicator(
+        key: _noDataRefreshIndicatorKey,
+        onRefresh: () async {
+          try {
+            await getAndSaveArchived(currentUser);
+            setState(() {
+              archivedCourses = getArchivedCourseListOf(currentUser.number);
+            });
+          } catch (e) {}
+        },
+        child: SingleChildScrollView(
+          physics: AlwaysScrollableScrollPhysics(),
+          child: Container(
+            height: MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top - 56,
+            child: Column(
+              children: <Widget>[
+                Flexible(
+                  flex: 0,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      left: max(sidePadding, 6),
+                      right: max(sidePadding, 6),
+                    ),
+                    child: AwardBar(0, 0, 0),
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      Strings.get(
+                        "no_archived_courses",
+                      ),
+                      style: Theme.of(context).textTheme.subhead,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -103,6 +150,7 @@ class _ArchivedCoursesPageState extends State<ArchivedCoursesPage>
 
   @override
   void afterFirstLayout(BuildContext context) {
-    _refreshIndicatorKey.currentState.show();
+    _noDataRefreshIndicatorKey.currentState?.show();
+    _haveDataRefreshIndicatorKey.currentState?.show();
   }
 }
