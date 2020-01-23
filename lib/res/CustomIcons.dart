7f4166b846b0b15fc546b60f218dca79b046cf17
pegasis <admin@pegasis.site>
2020-01-22 20:26:16 -0500

    + add calculator icon

--- lib/res/CustomIcons.dart
+++ lib/res/CustomIcons.dart
@@ -6,4 +6,5 @@ class CustomIcons {
   static const IconData fire = const IconData(0xe802, fontFamily: "CustomIcons");
   static const IconData award = const IconData(0xe803, fontFamily: "CustomIcons");
   static const IconData diamond = const IconData(0xf219, fontFamily: "CustomIcons");
+  static const IconData calculator = const IconData(0xe800, fontFamily: "CustomIcons2");
 }
